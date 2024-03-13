8adc14f84b71277d0eb62dcecc5c1bf781b40ec9
Maxim Sergeev <sergeev@rutoken.ru>
2024-03-13 18:31:04 +0300

    Add progress indicator dialog
    Close #28

---
+++ module.rutoken-tech/src/main/java/ru/rutoken/tech/ui/components/ProgressIndicatorDialog.kt
@@ -0,0 +1,64 @@
+package ru.rutoken.tech.ui.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import ru.rutoken.tech.ui.theme.RutokenTechTheme
+import ru.rutoken.tech.ui.utils.PreviewDark
+import ru.rutoken.tech.ui.utils.PreviewLight
+
+@Composable
+fun ProgressIndicatorDialog() {
+    Dialog(onDismissRequest = { /* Can't be dismissed manually */ }) {
+        Surface(
+            shape = RoundedCornerShape(28.dp),
+            color = MaterialTheme.colorScheme.surfaceContainerHigh
+        ) {
+            Box(
+                modifier = Modifier.padding(24.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier.size(48.dp),
+                    color = MaterialTheme.colorScheme.tertiary,
+                    strokeWidth = 3.dp,
+                    strokeCap = StrokeCap.Round
+                )
+
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .size(24.dp)
+                        .scale(scaleX = -1f, scaleY = 1f)
+                        .rotate(180f),
+                    color = MaterialTheme.colorScheme.primary,
+                    strokeWidth = 3.dp,
+                    strokeCap = StrokeCap.Round
+                )
+            }
+        }
+    }
+}
+
+@PreviewLight
+@PreviewDark
+@Composable
+fun ProgressIndicatorDialogPreview() {
+    RutokenTechTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            ProgressIndicatorDialog()
+        }
+    }
+}

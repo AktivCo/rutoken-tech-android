2ddf131337a795be947b958055f66b25a4bd7cce
Evgenii Potapov <potapov@rutoken.ru>
2025-05-16 09:29:25 +0300

    Up version to 1.2.1

--- module.rutoken-tech/build.gradle.kts
+++ module.rutoken-tech/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "ru.rutoken.tech"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.2"
+        versionCode = 8
+        versionName = "1.2.1"
 
         ndk {
             abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a"))
